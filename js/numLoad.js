295ffc51298a5ac3e09b05e41fb6fba948785a33
Shubham Vyas <shubhamvyas.indore@gmail.com>
2022-10-11 20:43:58 -0700

    script udpated | other figures added

--- js/numLoad.js
+++ js/numLoad.js
@@ -1,4 +1,5 @@
 function animateValue(obj, start, end, duration) {
+    
     let startTimestamp = null;
     const step = (timestamp) => {
       if (!startTimestamp) startTimestamp = timestamp;
@@ -11,5 +12,18 @@ function animateValue(obj, start, end, duration) {
     window.requestAnimationFrame(step);
   }
   
+  const random_num = Math.floor(Math.random() * 500)
+
   const obj = document.getElementById("value");
-  animateValue(obj, 100, 8, 5000);
+  animateValue(obj, random_num, 8, 1000);
+
+  const obj1 = document.getElementById("value1");
+  animateValue(obj1, random_num, 3743, 1000);
+
+  
+  const obj2 = document.getElementById("value2");
+  animateValue(obj2, random_num, 220, 1000);
+
+
+  const obj3 = document.getElementById("value3");
+  animateValue(obj3, 112000, 113527, 1000);
