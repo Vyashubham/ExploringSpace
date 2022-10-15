8c9eecb58c3729e4c1ce9c27a5fee6582c4f7bc1
Shubham Vyas <shubhamvyas.indore@gmail.com>
2022-10-15 16:14:16 -0700

    recentNews.js updated | script udpate req. for additional carousel items

--- js/recentNews.js
+++ js/recentNews.js
@@ -1,4 +1,4 @@
-const api_url = 'https://newsapi.org/v2/everything?q=space&apiKey=5589b7cfbd824b56b3f7122db64fc56d&pageSize=10&sortBy=popularity';
+const api_url = 'https://newsapi.org/v2/everything?q=astronomy&apiKey=5589b7cfbd824b56b3f7122db64fc56d&pageSize=10&sortBy=popularity';
 async function news(){
   const response = await fetch(api_url);
   const data = await response.json();
@@ -7,11 +7,11 @@ async function news(){
   console.log(data.articles); //working
   console.log(data.articles[0].urlToImage);
 //   document.getElementById('title1').textContent = data.articles[0].title;
-  document.getElementById('recentNewsImg').src = data.articles[1].urlToImage;
-  document.getElementById('recentNewsTitle').textContent = data.articles[1].title;
+  document.getElementById('recentNewsImg').src = data.articles[0].urlToImage;
+  document.getElementById('recentNewsTitle').textContent = data.articles[0].title;
 //   document.getElementById('recentNewsSource').textContent = data.articles[0].source;
-  document.getElementById('recentNewsDesc').textContent = data.articles[1].description;
-  document.getElementById('recentNewsReadNow').href = data.articles[1].url;
+  document.getElementById('recentNewsDesc').textContent = data.articles[0].description;
+  document.getElementById('recentNewsReadNow').href = data.articles[0].url;
   
 
 
