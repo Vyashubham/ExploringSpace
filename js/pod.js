7ed72365385588aac67e5693a72bf0baa22640f8
Shubham Vyas <shubhamvyas.indore@gmail.com>
2022-10-15 18:28:14 -0700

    pod.js created | using NASA APOD api to get data

---
+++ js/pod.js
@@ -0,0 +1,13 @@
+const pod_api_url = 'https://api.nasa.gov/planetary/apod?api_key=SVKO0rHRkpGdQ3zYjuWGvIXzjbKeICyjBRcJCBFZ';
+async function pod(){
+  const response = await fetch(pod_api_url);
+  const podData = await response.json();
+  console.log(podData);
+  console.log(podData.url)
+//   const imgUrl = data.url
+  document.getElementById('podImg').src = podData.url;
+  document.getElementById('podImgTitle').textContent = podData.title;
+  document.getElementById('podDesc').textContent = podData.explanation;
+
+}
+pod();
