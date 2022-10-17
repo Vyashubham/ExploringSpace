84b3080fc20b16fe7f764dc6b703f6fbebbb9541
Shubham Vyas <shubhamvyas.indore@gmail.com>
2022-10-17 00:10:52 -0700

    spacefolks.js created | using open-api

---
+++ js/spacefolks.js
@@ -0,0 +1,40 @@
+const spaceFolks_api_url = 'http://api.open-notify.org/astros.json';
+async function pod(){
+  const response = await fetch(spaceFolks_api_url);
+  const spaceFolks_data = await response.json();
+
+  console.log(spaceFolks_data);
+  const split = (spaceFolks_data.people.length)/2;
+
+//   for (let i = 0; i < split ; i++) {
+//     // console.log(spaceFolks_data.people[i].name);
+//     const para = document.createElement("p");
+//     const node = document.createTextNode(spaceFolks_data.people[i].name);
+//     // console.log(spaceFolks_data.people[i].name);
+//     para.appendChild(node);
+//     const element = document.getElementById("spacefolks1");
+//     element.appendChild(para);
+//   }
+
+//   for (let i = split; split < spaceFolks_data.people.length ; i++) {
+//     // console.log(spaceFolks_data.people[i].name);
+//     const para = document.createElement("p");
+//     const node = document.createTextNode(spaceFolks_data.people[i].name);
+//     // console.log(spaceFolks_data.people[i].name);
+//     para.appendChild(node);
+//     const element = document.getElementById("spacefolks2");
+//     element.appendChild(para);
+//   }
+    
+for (let i = 0; i < spaceFolks_data.people.length ; i++) {
+        // console.log(spaceFolks_data.people[i].name);
+        const para = document.createElement("p");
+        const node = document.createTextNode(spaceFolks_data.people[i].name);
+        // console.log(spaceFolks_data.people[i].name);
+        para.appendChild(node);
+        const element = document.getElementById("spacefolks1");
+        element.appendChild(para);
+      }
+    
+}
+pod();
