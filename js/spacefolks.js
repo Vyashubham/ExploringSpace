ebe6d189636e27a0b13a626a6e9400a0ef703a0e
Shubham Vyas <shubhamvyas.indore@gmail.com>
2022-10-18 17:42:51 -0700

    spacefolks.js updated | divided the api data into 2 section | testing needed

--- js/spacefolks.js
+++ js/spacefolks.js
@@ -6,35 +6,35 @@ async function pod(){
   console.log(spaceFolks_data);
   const split = (spaceFolks_data.people.length)/2;
 
-//   for (let i = 0; i < split ; i++) {
-//     // console.log(spaceFolks_data.people[i].name);
-//     const para = document.createElement("p");
-//     const node = document.createTextNode(spaceFolks_data.people[i].name);
-//     // console.log(spaceFolks_data.people[i].name);
-//     para.appendChild(node);
-//     const element = document.getElementById("spacefolks1");
-//     element.appendChild(para);
-//   }
+  for (let i = 0; i < split ; i++) {
+    // console.log(spaceFolks_data.people[i].name);
+    const para = document.createElement("p");
+    const node = document.createTextNode(spaceFolks_data.people[i].name);
+    // console.log(spaceFolks_data.people[i].name);
+    para.appendChild(node);
+    const element = document.getElementById("spacefolks1");
+    element.appendChild(para);
+  }
 
-//   for (let i = split; split < spaceFolks_data.people.length ; i++) {
-//     // console.log(spaceFolks_data.people[i].name);
-//     const para = document.createElement("p");
-//     const node = document.createTextNode(spaceFolks_data.people[i].name);
-//     // console.log(spaceFolks_data.people[i].name);
-//     para.appendChild(node);
-//     const element = document.getElementById("spacefolks2");
-//     element.appendChild(para);
-//   }
+  for (let i = split; split < spaceFolks_data.people.length ; i++) {
+    // console.log(spaceFolks_data.people[i].name);
+    const para = document.createElement("p");
+    const node = document.createTextNode(spaceFolks_data.people[i].name);
+    // console.log(spaceFolks_data.people[i].name);
+    para.appendChild(node);
+    const element = document.getElementById("spacefolks2");
+    element.appendChild(para);
+  }
     
-for (let i = 0; i < spaceFolks_data.people.length ; i++) {
-        // console.log(spaceFolks_data.people[i].name);
-        const para = document.createElement("p");
-        const node = document.createTextNode(spaceFolks_data.people[i].name);
-        // console.log(spaceFolks_data.people[i].name);
-        para.appendChild(node);
-        const element = document.getElementById("spacefolks1");
-        element.appendChild(para);
-      }
+// for (let i = 0; i < spaceFolks_data.people.length ; i++) {
+//         // console.log(spaceFolks_data.people[i].name);
+//         const para = document.createElement("p");
+//         const node = document.createTextNode(spaceFolks_data.people[i].name);
+//         // console.log(spaceFolks_data.people[i].name);
+//         para.appendChild(node);
+//         const element = document.getElementById("spacefolks1");
+//         element.appendChild(para);
+//       }
     
 }
 pod();
