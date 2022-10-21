298e3dd6be83a31dae4f0cb71e3437241fcbd3a0
Shubham Vyas <shubhamvyas.indore@gmail.com>
2022-10-21 05:10:11 -0700

    js formatted

--- js/iss.js
+++ js/iss.js
@@ -1,63 +1,48 @@
-const iss_api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
-var lat, lon, speed = 0;
-async function iss(){
+const iss_api_url = "https://api.wheretheiss.at/v1/satellites/25544";
+var lat,
+  lon,
+  speed = 0;
+async function iss() {
   const response = await fetch(iss_api_url);
   const issData = await response.json();
-  console.log(issData);
-  console.log(issData.url)
-  
-//   const para = document.createElement("p");
-//   const node = document.createTextNode(issData.name);
-//   const node1 = document.createTextNode(issData.latitude);
-//   // console.log(spaceFolks_data.people[i].name);
-//   para.appendChild(node);
-//   para.appendChild(node1);
-//   const element = document.getElementById("iss");
-//   element.appendChild(para);
-//   element.appendChild(para1);
+  // console.log(issData);
+  // console.log(issData.url);
 
-  console.log("YEYEYEY");
-  console.log("Lat");
+  // console.log("YEYEYEY");
+  // console.log("Lat");
   lat = issData.latitude;
-  console.log(lat);
+  // console.log(lat);
 
-  lon = issData.longitude
-  console.log("Lon");
-  console.log(issData.longitude);
+  lon = issData.longitude;
+  // console.log("Lon");
+  // console.log(issData.longitude);
 
   speed = issData.velocity;
-  console.log("velocity");
-  console.log(issData.velocity);
+  // console.log("velocity");
+  // console.log(issData.velocity);
 
-//   point_to_map(issData.latitude, issData.longitude);
-  const map_api_url = 'https://maps.googleapis.com/maps/api/staticmap?center='+lat+','+lon+'&zoom=2&size=800x800&markers=size:large%7Ccolor:blue%7Clabel:ISS%7C'+lat+','+lon+'&maptype=satellite&key=AIzaSyAtIfPCwFuYFkPCHY65nv5EHwQJDz6IKJo';
-    document.getElementById('issnow').src = map_api_url;
-  console.log(map_api_url);
-  console.log("YOO");
-//   console.log(Math.round((lat + Number.EPSILON) * 100) / 100);
-lat = +lat.toFixed(5);
-lon = +lon.toFixed(5);
-speed = +speed.toFixed(4)
-console.log(lat);
+  //   point_to_map(issData.latitude, issData.longitude);
+  const map_api_url =
+    "https://maps.googleapis.com/maps/api/staticmap?center=" +
+    lat +
+    "," +
+    lon +
+    "&zoom=2&size=800x800&markers=size:large%7Ccolor:blue%7Clabel:ISS%7C" +
+    lat +
+    "," +
+    lon +
+    "&maptype=satellite&key=AIzaSyAtIfPCwFuYFkPCHY65nv5EHwQJDz6IKJo";
+  document.getElementById("issnow").src = map_api_url;
+  // console.log(map_api_url);
+  // console.log("YOO");
+
+  lat = +lat.toFixed(5);
+  lon = +lon.toFixed(5);
+  speed = +speed.toFixed(4);
+  // console.log(lat);
   document.getElementById("iss-lat").innerHTML = lat;
   document.getElementById("iss-lon").innerHTML = lon;
   document.getElementById("iss-speed").innerHTML = speed;
 }
-
-// async function point_to_map(lon, lan){
-//     // const map_api_url = 'https://api.opencagedata.com/geocode/v1/json?q=q='+ lan +','+ lon +'&key=13784334c5d14486afa7d49f2731e95d&pretty=1';
-//     const map_api_url = 'https://maps.googleapis.com/maps/api/staticmap?center='+lat+','+lon+'&zoom=12&size=400x400&key=AIzaSyAtIfPCwFuYFkPCHY65nv5EHwQJDz6IKJo';
-//     console.log(map_api_url);
-    
-//     const response = await fetch(map_api_url);
-//      const mapData = await response.json();
-//     console.log(mapData);
-//     console.log(mapData.results[0].annotations.OSM.url);
-
-//     document.getElementById('issnow').src = mapData.results[0].annotations.OSM.url;
-
-  
-
-// }
 iss();
-// point_to_map();
+
--- js/recentNews.js
+++ js/recentNews.js
@@ -1,5 +1,6 @@
-const api_url = 'https://newsapi.org/v2/everything?q=astronomy&apiKey=5589b7cfbd824b56b3f7122db64fc56d&pageSize=10&sortBy=recent';
-async function news(){
+const api_url =
+  "https://newsapi.org/v2/everything?q=astronomy&apiKey=5589b7cfbd824b56b3f7122db64fc56d&pageSize=10&sortBy=recent";
+async function news() {
   const response = await fetch(api_url);
   const data = await response.json();
   // console.log(data);
@@ -7,21 +8,26 @@ async function news(){
   // console.log(data.articles); //working
   // console.log(data.articles[0].urlToImage);
   // document.getElementById('title1').textContent = data.articles[0].title;
-  document.getElementById('recentNewsImg0').src = data.articles[0].urlToImage;
-  document.getElementById('recentNewsTitle0').textContent = data.articles[0].title;
-//   document.getElementById('recentNewsSource').textContent = data.articles[0].source;
-  document.getElementById('recentNewsDesc0').textContent = data.articles[0].description;
-  document.getElementById('recentNewsReadNow0').href = data.articles[0].url;
-  
-  document.getElementById('recentNewsImg1').src = data.articles[1].urlToImage;
-  document.getElementById('recentNewsTitle1').textContent = data.articles[1].title;
-  document.getElementById('recentNewsDesc1').textContent = data.articles[1].description;
-  document.getElementById('recentNewsReadNow1').href = data.articles[1].url;
-  
-  document.getElementById('recentNewsImg2').src = data.articles[2].urlToImage;
-  document.getElementById('recentNewsTitle2').textContent = data.articles[2].title;
-  document.getElementById('recentNewsDesc2').textContent = data.articles[2].description;
-  document.getElementById('recentNewsReadNow2').href = data.articles[2].url;
+  document.getElementById("recentNewsImg0").src = data.articles[0].urlToImage;
+  document.getElementById("recentNewsTitle0").textContent =
+    data.articles[0].title;
+  //   document.getElementById('recentNewsSource').textContent = data.articles[0].source;
+  document.getElementById("recentNewsDesc0").textContent =
+    data.articles[0].description;
+  document.getElementById("recentNewsReadNow0").href = data.articles[0].url;
 
+  document.getElementById("recentNewsImg1").src = data.articles[1].urlToImage;
+  document.getElementById("recentNewsTitle1").textContent =
+    data.articles[1].title;
+  document.getElementById("recentNewsDesc1").textContent =
+    data.articles[1].description;
+  document.getElementById("recentNewsReadNow1").href = data.articles[1].url;
+
+  document.getElementById("recentNewsImg2").src = data.articles[2].urlToImage;
+  document.getElementById("recentNewsTitle2").textContent =
+    data.articles[2].title;
+  document.getElementById("recentNewsDesc2").textContent =
+    data.articles[2].description;
+  document.getElementById("recentNewsReadNow2").href = data.articles[2].url;
 }
 news();
