16879046ec54eeb38fb2b2870fc899f0588a6b4c
Shubham Vyas <shubhamvyas.indore@gmail.com>
2022-10-21 05:24:27 -0700

    recentNews query updated

--- js/recentNews.js
+++ js/recentNews.js
@@ -1,22 +1,30 @@
 const api_url =
-  "https://newsapi.org/v2/everything?q=astronomy&apiKey=5589b7cfbd824b56b3f7122db64fc56d&pageSize=10&sortBy=recent";
+  "https://newsapi.org/v2/everything?q=astronomy&apiKey=5589b7cfbd824b56b3f7122db64fc56d&pageSize=10&SortBy=publishedAt";
+  // "https://newsapi.org/v2/everything?q=astronomy&apiKey=5589b7cfbd824b56b3f7122db64fc56d&pageSize=10&sortBy=recent"
 async function news() {
   const response = await fetch(api_url);
   const data = await response.json();
-  // console.log(data);
+  console.log(data);
   // console.log(data.status); // working
   // console.log(data.articles); //working
   // console.log(data.articles[0].urlToImage);
   // document.getElementById('title1').textContent = data.articles[0].title;
   document.getElementById("recentNewsImg0").src = data.articles[0].urlToImage;
+  if(document.getElementById("recentNewsImg0").style.src = "null"){
+    document.getElementById("recentNewsImg0").src = "https://images.unsplash.com/photo-1588681664899-f142ff2dc9b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80";
+  }
   document.getElementById("recentNewsTitle0").textContent =
     data.articles[0].title;
-  //   document.getElementById('recentNewsSource').textContent = data.articles[0].source;
+    // document.getElementById('recentNewsSource').textContent = data.articles[0].source.name;
+    // console.log(data.articles[0].source.name);
   document.getElementById("recentNewsDesc0").textContent =
     data.articles[0].description;
   document.getElementById("recentNewsReadNow0").href = data.articles[0].url;
 
   document.getElementById("recentNewsImg1").src = data.articles[1].urlToImage;
+  if(document.getElementById("recentNewsImg1").style.src = "null"){
+    document.getElementById("recentNewsImg0").src = "https://images.unsplash.com/photo-1588681664899-f142ff2dc9b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80";
+  }
   document.getElementById("recentNewsTitle1").textContent =
     data.articles[1].title;
   document.getElementById("recentNewsDesc1").textContent =
@@ -24,6 +32,9 @@ async function news() {
   document.getElementById("recentNewsReadNow1").href = data.articles[1].url;
 
   document.getElementById("recentNewsImg2").src = data.articles[2].urlToImage;
+  if(document.getElementById("recentNewsImg2").style.src = "null"){
+    document.getElementById("recentNewsImg0").src = "https://images.unsplash.com/photo-1588681664899-f142ff2dc9b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80";
+  }
   document.getElementById("recentNewsTitle2").textContent =
     data.articles[2].title;
   document.getElementById("recentNewsDesc2").textContent =
